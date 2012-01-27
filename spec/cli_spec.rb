35709d566d30f4e67cb3cc1a1242e4718f5114f3
Andrew Cox <andrew.cox@autodesk.com>
2012-01-27 17:38:46 -0500

    Cleaning up all specs

---
+++ spec/cli_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+include PivotalTracker
+
+describe Cli do
+  context "#import" do
+    # it "imports stories from a JIRA project into Pivotal project" do
+    #   cmd = Command.new
+    #   cmd.import("id" => "369409")
+    #   
+    # end
+  end
+end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -5,6 +5,7 @@ include PivotalTracker
 describe Command do
   context "#import" do
     it "imports stories from a JIRA project into Pivotal project" do
+      pending
       cmd = Command.new
       cmd.import("id" => "369409", "jira_id" => "FFM")
     end
